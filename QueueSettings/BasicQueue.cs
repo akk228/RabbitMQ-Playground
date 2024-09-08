334b6ce2193d881bd478bfadee6cbf063298fab3
Andrei Kovtun <andreykpq@gmail.com>
2024-09-08 19:52:08 +0200

    Adds basic queue settings

---
+++ QueueSettings/BasicQueue.cs
@@ -0,0 +1,11 @@
+﻿using RabbitMQ.Client;
+namespace QueueSettings;
+
+public class BasicQueue
+{
+    public string Name => "MyFirstQueue";
+    public bool IsDurable => false;
+    public bool IsExclusive => false;
+    public bool AutoDelete => false;
+    public IDictionary<string, object> Arguments { get; set; }
+}
