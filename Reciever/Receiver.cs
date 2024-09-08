ef13e921865f22a02c1b600a62bc4c165b0276ad
Andrei Kovtun <andreykpq@gmail.com>
2024-09-08 20:14:36 +0200

    Create Receiver

---
+++ Reciever/Receiver.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using QueueSettings;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+using (var connection = connectionFactory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        var queue = new BasicQueue();
+        
+        channel.QueueDeclare(
+            queue.Name,
+            queue.IsDurable,
+            queue.IsExclusive,
+            queue.AutoDelete,
+            queue.Arguments
+        );
+
+        var consumer = new EventingBasicConsumer(channel);
+        
+        consumer.Received += (sender, eventArgs) =>
+        {
+            var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+            Console.WriteLine($"Message: {message}, was received");
+        };
+        
+        channel.BasicConsume(queue.Name, true, consumer);
+        Console.ReadKey();
+    }
+}
