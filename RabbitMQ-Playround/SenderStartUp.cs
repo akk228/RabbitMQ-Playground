f1a9b39b69046e4e376c4f771321b0fb1b80037a
Andrei Kovtun <andreykpq@gmail.com>
2024-09-08 19:52:43 +0200

    Finishes sender's set up

--- RabbitMQ-Playround/SenderStartUp.cs
+++ RabbitMQ-Playround/SenderStartUp.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
+using QueueSettings;
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
 
+using (var connection = connectionFactory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        // use just Named queue
+        var queue = new BasicQueue();
+        
+        channel.QueueDeclare(
+            queue.Name,
+            queue.IsDurable,
+            queue.IsExclusive,
+            queue.AutoDelete,
+            queue.Arguments
+        );
+        
+        var message = "Wazzap!";
+        var body = Encoding.UTF8.GetBytes(message);
+        
+        channel.BasicPublish("", routingKey: queue.Name, mandatory: false, body: body);
+        Console.WriteLine($"Message: {message} was Published");
+    }
+}
 Console.ReadKey();
