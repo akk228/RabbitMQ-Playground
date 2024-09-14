a2e5212d6820df0f301ac62376132854b9cbd11e
Andrei Kovtun <andreykpq@gmail.com>
2024-09-14 22:35:41 +0200

    Updates receiver to create several consumers

--- Reciever/Receiver.cs
+++ Reciever/Receiver.cs
@@ -1,43 +1,40 @@
 ﻿/*
  * I will create here application that will create the amount of consumers that you specify,
  * and these consumers will be consuming messages in a round-robin way
- * meaning that they are gonna be receiving messages one after another in a circle manner
+ * meaning that they are gonna be receiving messages one after another in a circle manner.
+ * Also, for the sake of being more realistic we will give every consumer its own connection
  */
 using System.Text;
 using QueueSettings;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using Reciever;
 
 var connectionFactory = new ConnectionFactory()
 {
     HostName = "localhost"
 };
 
-using (var connection = connectionFactory.CreateConnection())
-{
-    using (var channel = connection.CreateModel())
-    {
-        var queue = new BasicQueue();
-        
-        channel.QueueDeclare(
-            queue.Name,
-            queue.IsDurable,
-            queue.IsExclusive,
-            queue.AutoDelete,
-            queue.Arguments
-        );
+var consumerCount = 2;
 
-        var consumer = new EventingBasicConsumer(channel);
-        
-        consumer.Received += (sender, eventArgs) =>
-        {
-            var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
-            Console.WriteLine($"Message: {message}, was received");
-        };
-        
-        channel.BasicConsume(queue.Name, true, consumer);
-        
-        Console.WriteLine("Press [enter] to exit.");
-        Console.ReadKey();
-    }
+Console.WriteLine("Enter the number of consumers you wanna run : ");
+try {
+    consumerCount = int.Parse(Console.ReadLine());
 }
+catch (Exception e) {
+}
+
+var consumers = new Task[consumerCount];
+
+var userInput = new TaskCompletionSource();
+
+for (int i = 0; i < consumerCount; i++) {
+    consumers[i] = Consumer.Run(i, userInput);
+}
+
+Console.ReadKey();
+
+userInput.SetResult();
+
+await Task.WhenAll(consumers);
+
