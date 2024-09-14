b42bc0d2d9ed1dfd9f328e7da0ca34f0a211fbba
Andrei Kovtun <andreykpq@gmail.com>
2024-09-14 20:28:59 +0200

    Makes queue exclusive

--- QueueSettings/BasicQueue.cs
+++ QueueSettings/BasicQueue.cs
@@ -5,7 +5,7 @@ public class BasicQueue
 {
     public string Name => "MyFirstQueue";
     public bool IsDurable => false;
-    public bool IsExclusive => false;
+    public bool IsExclusive => true;
     public bool AutoDelete => false;
     public IDictionary<string, object> Arguments { get; set; }
 }
