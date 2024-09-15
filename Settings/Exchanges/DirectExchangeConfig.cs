ebb337ac9a42fecadda6d8b3f8ccd20d37eeccde
Andrei Kovtun <andreykpq@gmail.com>
2024-09-15 11:16:13 +0200

    Adds configs for greetings exchange

---
+++ Settings/Exchanges/DirectExchangeConfig.cs
@@ -0,0 +1,12 @@
+﻿using RabbitMQ.Client;
+
+namespace QueueSettings.Exchanges;
+
+public class DirectExchangeConfig
+{
+    public string Name => "Greetings-Exchange";
+    public string Type => ExchangeType.Direct;
+    public bool Durable => false;
+    public bool AutoDelete => true;
+    public IDictionary<string, object> Arguments { get; set; }
+}
