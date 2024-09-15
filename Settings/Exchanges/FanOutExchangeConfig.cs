78788a16e7e2e7d416c020698f0e75a49aa03031
Andrei Kovtun <andreykpq@gmail.com>
2024-09-15 10:45:48 +0200

    Adds exchange to settings, and updats publisher to publish in an exchange

---
+++ Settings/Exchanges/FanOutExchangeConfig.cs
@@ -0,0 +1,12 @@
+﻿using RabbitMQ.Client;
+
+namespace QueueSettings.Exchanges;
+
+public class FanOutExchangeConfig
+{
+    public string Name => "simple-fan-out";
+    public string Type => ExchangeType.Fanout;
+    public bool Durable => false;
+    public bool AutoDelete => true;
+    public IDictionary<string, object> Arguments { get; set; }
+}
