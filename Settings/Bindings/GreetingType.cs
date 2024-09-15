bd429634a4ebeffe411c7b68575fa9bbcac1c49a
Andrei Kovtun <andreykpq@gmail.com>
2024-09-15 11:14:34 +0200

    Adds bindings

---
+++ Settings/Bindings/GreetingType.cs
@@ -0,0 +1,11 @@
+﻿namespace QueueSettings.Bindings;
+
+/// <summary>
+/// We use here different greetings as bindings between queue and exchange
+/// </summary>
+public class GreetingType
+{
+    public const string Greeting = "Hello!";
+    public const string Farewell = "Goodbye!";
+    public const string InformalFarewell = "Fuck off, bitch!";
+}
